9fec26a0c0ecf208e2036b3f11d5977850394a3c
conradsuuna <conradsuuna.cs@gmail.com>
2022-02-26 20:21:38 +0300

    project initialisation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AuthComponent } from './components/auth/auth.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { BaseComponent } from './components/base/base.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    AuthComponent,
+    LoginComponent,
+    RegisterComponent,
+    BaseComponent,
   ],
   imports: [
     BrowserModule,
