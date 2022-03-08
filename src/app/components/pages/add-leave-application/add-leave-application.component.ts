5e2be92c52dc257f5639c1fe73827464579e441b
phibi3 <40600865+phibi3@users.noreply.github.com>
2022-03-08 09:17:48 +0300

    address groups

--- src/app/components/pages/add-leave-application/add-leave-application.component.ts
+++ src/app/components/pages/add-leave-application/add-leave-application.component.ts
@@ -12,4 +12,26 @@ export class AddLeaveApplicationComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  add() {
+    let addressRow = document.createElement('div');
+      addressRow.className = 'row';
+      addressRow.innerHTML = `
+      <hr>
+      <div class="col-6 form-group">
+        <label>Address Details</label>
+        <input type="text" class="form-control" placeholder="eg. ntinda,kampala">
+      </div>
+
+      <div class="col-6 form-group">
+        <label>Telephone</label>
+        <input type="tel" class="form-control" placeholder="eg. +256 770088007">
+      </div>
+
+      <div class="col-6 form-group">
+        <label>Next of Kin Contact</label>
+        <input type="text" class="form-control" placeholder="eg. +256 770088007">
+      </div>`;
+      document.querySelector('.displayFields').appendChild(addressRow);
+  }
+
 }
